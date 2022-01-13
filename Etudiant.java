7f1b26263220061dccc50e60df34a0450477e6b0
salahed1 <96252447+salahed1@users.noreply.github.com>
2022-01-13 13:32:04 +0100

    Add files via upload

---
+++ Etudiant.java
@@ -0,0 +1,15 @@
+package ma.education.tp2.statickeyword;
+
+public class Etudiant {
+	long id;
+	String nom;
+	public static int nbEtudiants;
+	
+	public Etudiant (long id, String nom,int nb) {
+		this.id = id;
+		this.nom = nom;
+		nbEtudiants+=nb;
+		}
+	
+
+}
